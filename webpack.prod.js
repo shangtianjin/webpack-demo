ec3bc4aba9f454611db2e8980c8e70c125d49372
rain <rain.late@outlook.com>
2019-11-06 15:23:20 +0800

    config

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,12 @@
 const webpack = require('webpack')
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyjsWebpackPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const px2rem = require('postcss-px2rem')
+
 
 module.exports = {
   entry: {
@@ -27,7 +33,16 @@ module.exports = {
       },
       {
         test: /.less$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader']
+        use: [
+          MiniCssExtractPlugin.loader, 
+          'css-loader', 
+          'less-loader',
+          'postcss-loader'
+          // {
+          //   loader: 'px2rem-loader',
+
+          // }
+        ]
       },
       // {
       //   test: /.(png|jpg|gif|jpeg)$/,
@@ -48,8 +63,45 @@ module.exports = {
     poll: 1000
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: '[name]_[contenthash:8].css'
+    }),
+    new UglifyjsWebpackPlugin({
+      test: /.js($|\?)/i,
+      cache: false
+    }),
+    new OptimizeCssAssetsWebpackPlugin({
+      cssProcessor: require('cssnano'),
+      assetNameRegExp: /\.css$/g,
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, `src/index.html`),
+      filename: 'index.html',
+      chunks: ['index'],
+      inject: true,
+      minify: {
+        html5: true,
+        collapseWhitespace: true,
+        preserveLineBreaks: false,
+        minifyCSS: true,
+        minifyJS: true,
+        removeComments: false
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, `src/search.html`),
+      filename: 'search.html',
+      chunks: ['search'],
+      inject: true,
+      minify: {
+        html5: true,
+        collapseWhitespace: true,
+        preserveLineBreaks: false,
+        minifyCSS: true,
+        minifyJS: true,
+        removeComments: false
+      }
     })
   ]
 }
